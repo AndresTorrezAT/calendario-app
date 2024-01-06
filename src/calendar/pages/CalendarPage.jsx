5ca5b926abc520e9501ef155293639eaaa769938
[Andres] <[andres.torrez.work@gmail.com]>
2024-01-06 06:51:51 -0400

    seccion 22-08 terminado

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,55 @@
-import { Navbar } from "../";
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { addHours } from 'date-fns';
+import { Navbar } from '../';
+
+import { localizer, getMessagesES } from '../../helpers';
+
+const events = [{
+  title: 'Cumpleaños de jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Fernando',
+  }
+}];
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }} //este calculo es para que el calendario quede fijo en la pantalla sin espacion para scrolear
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+      />
+
     </>
   )
 }
